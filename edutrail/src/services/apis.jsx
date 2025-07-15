835bec08c1988b9c30e3ef9cab92882c079bd41a
Aryanbhong <aryanbhong20@gmail.com>
2025-07-16 00:33:01 +0530

    url change

--- edutrail/src/services/apis.jsx
+++ edutrail/src/services/apis.jsx
@@ -1,5 +1,5 @@
 
-const BASE_URL ="http://localhost:3000/api/v1"
+const BASE_URL ="https://edutrail-2.onrender.com/api/v1"
 export const categories = {
   CATEGORIES_API: BASE_URL + "/course/showAllCategories",
 }
